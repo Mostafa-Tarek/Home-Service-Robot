e478f960ada78bb2a4713712171f345866a0f73d
Mostafa-Tarek <46390321+Mostafa-Tarek@users.noreply.github.com>
2021-04-03 18:53:59 +0200

    Update shell file

--- script/add_makers.sh
+++ script/add_makers.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch"&
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find map)/worlds/U_building.world"&
 sleep 5
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch"&
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=$(rospack find map)/maps/map.yaml"&
 sleep 5
 xterm -e "roslaunch turtlebot_rviz_launchers view_teleop_navigation.launch"&
 sleep 5
-xterm -e "rosrun add_makers add_makers"
+xterm -e "rosrun add_makers add_marker_blinking"
